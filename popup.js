5997d23ad1ccd28d3f7dccef54cbef9fba997203
Callum Maybon <callum.maybon@gmail.com>
2020-12-05 12:15:12 +1100

    Intial commit

---
+++ popup.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var blacklist = [
+        "UCMOgdURr7d8pOVlc-alkfRg"
+    ]
+    var checkChannelsButton = document.getElementById('checkChannels');
+    checkChannelsButton.addEventListener('click', function () {
+        
+    })
+})
