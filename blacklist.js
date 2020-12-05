b7759bc27e5813b539487c6f4a0a23ae7bb1895a
Callum Maybon <callum.maybon@gmail.com>
2020-12-05 14:08:31 +1100

    Youtube home compatibility

--- blacklist.js
+++ blacklist.js
@@ -2,10 +2,34 @@ var blacklist = [
     "Northernlion",
     "Frisson",
     "Unlike Pluto",
-    "Apashe"
+    "Apashe",
+    "xQcOW",
+    "OfflineTV",
+    "Linus Tech Tips",
+    "jackfrags",
+    "PewDiePie",
+    "Kitchen Nightmares",
+    "Disguised Toast",
+    "jacksepticeye",
+    "xKito Music",
+    "Sound Cloudx",
+    "Gordon Ramsay",
+    "First We Feast",
+    "Welyn",
+    "CloudKid",
+    "Everything Pokelawls",
+    "JxmyHighroller",
+    "SahaKit",
+    "Ghrey",
+    "ABC News In-depth"
 ]
 
 var videos = document.getElementsByClassName("style-scope ytd-compact-video-renderer");
+if (videos === null || videos.length === 0) {
+    console.log("Using rich-grid");
+    videos = document.getElementById("contents").childNodes;
+}
+
 for (let element of videos) {
     var thumbnail = element.querySelector("#thumbnail");
     if (thumbnail === null) {
@@ -14,12 +38,8 @@ for (let element of videos) {
 
     var videoWatchId = thumbnail.getAttribute("href").split("/watch?v=")[1]
     var videoInfo = element.querySelector("#text");
-    if (videoInfo === null) {
-        continue;
-    }
     
     var channelName = videoInfo.textContent;
-    console.log(channelName);
 
     if (blacklist.indexOf(channelName) >= 0) {
         console.log("blacklisted channel found '" + channelName + "', video id: " + videoWatchId)
